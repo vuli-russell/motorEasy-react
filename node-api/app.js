4a5bf1600e41306db9e19f94e30c164b24c79528
unknown <vulirussell@gmail.com>
2020-11-28 22:16:42 +0000

    added minimal error catching to api

--- node-api/app.js
+++ node-api/app.js
@@ -10,12 +10,19 @@ app.use(cors())
 const pageSize = 3;
 
 app.get("/getTyres", async (request,response) => {
-    const page = request.query.p;
+    let page = parseInt(request.query.p);
     const searchStr = request.query.q;
-//validate page -> must be 1 or more
-
-    const data = await mongoGet(searchStr,page,pageSize)
-    response.send(data)
+    if(page){
+        const data = await mongoGet(searchStr,page,pageSize)
+        if(data==="ServerError"){
+            response.status(500).send("Error retriving data from database")
+        }else{
+            response.send(data)
+        }
+    }else{
+        response.status(400).send("p (page) query parameter must be a parseable as an integer greater than 1")
+    }
+    
 })
 
 app.listen(8080);
--- node-api/services/mongodbService.js
+++ node-api/services/mongodbService.js
@@ -8,18 +8,23 @@ const connectionString = `mongodb+srv://${process.env.MONGO_USER}:${process.env.
 const mongoClient = mongodb.MongoClient;
 
 export const mongoGet = async(searchStr,page,pageSize) => {
-    const client = await mongoClient.connect(connectionString, { useUnifiedTopology: true })
-    const cursor = await client.db("motorEasyTechTest").collection("tyres")
-        .find({$or:
-            [
-                {title: new RegExp(searchStr,"i")},
-                {size: new RegExp(searchStr,"i")}
-            ]
-        })
-        //should be a sort here to ensure same order each time
-        .skip((page-1)*pageSize)
-        .limit(3)
-        .toArray()
-    await client.close()
-    return cursor
+    try {
+        const client = await mongoClient.connect(connectionString, { useUnifiedTopology: true })
+        const response = await client.db("motorEasyTechTest").collection("tyres")
+            .find({$or:
+                [
+                    {title: new RegExp(searchStr,"i")},
+                    {size: new RegExp(searchStr,"i")}
+                ]
+            })
+            //should be a sort here to ensure same order each time
+            .skip((page-1)*pageSize)
+            .limit(3)
+            .toArray()
+            await client.close()
+            return response
+    } catch(e){
+        console.log(e)
+        return "ServerError"
+    }
 }
