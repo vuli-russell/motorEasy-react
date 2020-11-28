4b87f52df0f235148c6bc8c6831c611eb3c7915e
unknown <vulirussell@gmail.com>
2020-11-28 23:53:34 +0000

    added set current page call to Search and mocked version to search test

--- react-app/src/components/Search/Search.test.js
+++ react-app/src/components/Search/Search.test.js
@@ -6,11 +6,12 @@ describe("Search Tests", () =>{
   let searchComponent;
   const setSearchStrMock = jest.fn();
   const setHasSearchedMock = jest.fn();
+  const setCurrentPageMock = jest.fn()
   const testSearchString = "test";
   
 
   beforeEach(()=>{
-    searchComponent = shallow(<Search setSearchStr={setSearchStrMock} setHasSearched={setHasSearchedMock}/>);
+    searchComponent = shallow(<Search setSearchStr={setSearchStrMock} setHasSearched={setHasSearchedMock} setCurrentPage={setCurrentPageMock}/>);
     setSearchStrMock.mockClear();
   });
   
@@ -37,5 +38,13 @@ describe("Search Tests", () =>{
     searchComponent.find("button").simulate("click")
     expect(setHasSearchedMock).toHaveBeenCalledTimes(1);
     expect(setHasSearchedMock).toHaveBeenCalledWith(true);
+  });
+
+  it("should call setCurrentPage with 1 on button click", () => {
+    expect(setHasSearchedMock).toHaveBeenCalledTimes(0);
+    searchComponent.find("input").simulate("change", {target:{value: testSearchString}})
+    searchComponent.find("button").simulate("click")
+    expect(setCurrentPageMock).toHaveBeenCalledTimes(1);
+    expect(setCurrentPageMock).toHaveBeenCalledWith(1);
   });
 });  
