4e817149d7da9b857f2528d83a98c85ec7e1ff8b
unknown <vulirussell@gmail.com>
2020-11-29 11:54:00 +0000

    added stars to card

--- react-app/src/components/TyreList/TyreCard/TyreCard.jsx
+++ react-app/src/components/TyreList/TyreCard/TyreCard.jsx
@@ -1,15 +1,44 @@
 import React from "react";
 import styles from "./TyreCard.module.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar as faSolidStar } from "@fortawesome/free-solid-svg-icons";
+import { faStar as faOutlineStar } from "@fortawesome/free-regular-svg-icons";
 
 const TyreCard = (props) => {
   // console.log(props.tyre)
-  const { brand, size, image, amazon_link, model, price, rating, title } = props.tyre 
+  const { brand, size, image, amazon_link, model, price, title } = props.tyre;
+  const rating = props.tyre.rating.match(/^\d\.\d/)[0];
+
+  const fullStarsJSX = (
+    <div className={styles.fullStars} style={{width: `${rating}em`}}>
+      <FontAwesomeIcon icon={faSolidStar} />
+      <FontAwesomeIcon icon={faSolidStar} />
+      <FontAwesomeIcon icon={faSolidStar} />
+      <FontAwesomeIcon icon={faSolidStar} />
+      <FontAwesomeIcon icon={faSolidStar} />
+    </div>
+  )
+
+  const emptyStarsJSX = (
+    <div className={styles.emptyStars}>
+      <FontAwesomeIcon icon={faSolidStar} />
+      <FontAwesomeIcon icon={faSolidStar} />
+      <FontAwesomeIcon icon={faSolidStar} />
+      <FontAwesomeIcon icon={faSolidStar} />
+      <FontAwesomeIcon icon={faSolidStar} />
+    </div>
+  )
+
   return (
     <div className={styles.tyreCard}>
       <div className={styles.imgDiv}>
         <img src={image} alt={title}/>
       </div>
-      {rating.match(/^\d\.\d/)[0]}
+      <div className={styles.starsDiv}>
+        {fullStarsJSX}
+        {emptyStarsJSX}
+      </div>
+      
       <div className={styles.titleDiv}>
         <h2>{title}</h2>
         <h2 className={styles.price}>{price}</h2>
