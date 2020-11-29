9b4e02f3b9e1ac091ebf6ca7ff3c638e96109c50
unknown <vulirussell@gmail.com>
2020-11-29 00:28:57 +0000

    some very basic styling done on cards

--- react-app/src/components/TyreList/TyreCard/TyreCard.jsx
+++ react-app/src/components/TyreList/TyreCard/TyreCard.jsx
@@ -2,11 +2,23 @@ import React from "react";
 import styles from "./TyreCard.module.scss";
 
 const TyreCard = (props) => {
-  const { brand, size } = props.tyre
+  // console.log(props.tyre)
+  const { brand, size, image, amazon_link, model, price, rating, title } = props.tyre 
   return (
-    <div>
-      <h1>{brand}</h1>
-      <h1>{size}</h1>
+    <div className={styles.tyreCard}>
+      <div className={styles.imgDiv}>
+        <img src={image} alt={title}/>
+      </div>
+      {rating.match(/^\d\.\d/)[0]}
+      <div className={styles.titleDiv}>
+        <h2>{title}</h2>
+        <h2 className={styles.price}>{price}</h2>
+      </div>
+      <h2>Size: {size}</h2>
+      <div className={styles.amazonLink}>
+        <a href={amazon_link} target="_blank" rel="noopener noreferrer">Amazon Link</a>
+      </div>
+      
     </div>
   );
 };
